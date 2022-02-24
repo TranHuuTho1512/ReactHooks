5805957637be29a96b364f4673efd5ea2e7b5574
Thọ Trần Hữu <thotran151200@gmail.com>
2022-02-24 11:26:23 +0700

    cleanup Hooks

--- src/customize/fetch.js
+++ src/customize/fetch.js
@@ -6,9 +6,14 @@ const useFetch = (url) => {
     const [isLoading, setIsLoading] = useState(true)
     const [isError, setIsError] = useState(false)
     useEffect(async () => {
-        try {
-            async function fetchData() {
-                let res = await axios.get(url)
+
+        const ourRequest = axios.CancelToken.source() // <-- 1st step
+        async function fetchData() {
+            try {
+
+                let res = await axios.get(url, {
+                    cancelToken: ourRequest.token, // <-- 2nd step
+                })
                 let data = res && res.data ? res.data : [];
                 if (data && data.length > 0) {
                     data.map(item => {
@@ -20,16 +25,28 @@ const useFetch = (url) => {
                 setData(data);
                 setIsLoading(false);
                 setIsError(false);
+
             }
+            catch (err) {
+                if (axios.isCancel(err)) {
+                    console.log('Request canceled', err.message);
+                } else {
+                    setIsError(true);
+                    setIsLoading(false);
+
+                }
+
+            }
+        }
+        setTimeout(() => {
             fetchData();
-        }
-        catch (e) {
-            setIsError(true);
-            setIsLoading(false);
+        }, 3000);
 
+        return () => {
+            ourRequest.cancel('Operation canceled by the user.');
         }
 
-    }, []);
+    }, [url]);
     return {
         data, isLoading, isError
     }
