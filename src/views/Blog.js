7a0f6adad1535906ab398929912e1b23112e3698
Thọ Trần Hữu <thotran151200@gmail.com>
2022-02-25 00:39:11 +0700

    post/delete data

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,32 +1,68 @@
 import useFetch from "../customize/fetch";
 import './Blog.scss';
 import { Link, useHistory } from "react-router-dom";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { useState, useEffect } from "react";
+import AddNewBlog from './AddNewBlog';
 
 const Blog = () => {
+
+    const [show, setShow] = useState(false);
+    const [newData, setNewData] = useState([]);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     let history = useHistory();
 
     const { data: dataBlogs, isLoading, isError }
         = useFetch(`https://jsonplaceholder.typicode.com/posts`, false);
 
-    let newData = [];
-    if (dataBlogs && dataBlogs.length > 0) {
-        newData = dataBlogs.slice(0, 9)
+
+    useEffect(() => {
+        if (dataBlogs && dataBlogs.length > 0) {
+            let data = dataBlogs.slice(0, 9);
+            setNewData(data);
+        }
+    }, [dataBlogs])
+
+    const handleAddNew = (blog) => {
+
+        let data = newData;
+        data.unshift(blog);
+        setShow(false);
+        setNewData(data);
 
     }
-
-    const handleAddNew = () => {
-        history.push('/add-new-blog');
+    const deletePost = (id) => {
+        let data = newData;
+        data = data.filter(item => item.id !== id)
+        setNewData(data);
     }
 
     return (
         <>
-            <div><button className="btn-add-new" onClick={handleAddNew}>+ Add new blog</button></div>
+            <Button variant="primary" className="my-3" onClick={handleShow}>
+                + Add New Blog
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add New Blog</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <AddNewBlog handleAddNew={handleAddNew} />
+                </Modal.Body>
+            </Modal>
+
             <div className="blogs-container" >
                 {isLoading === false && newData && dataBlogs.length > 0 && newData.map(item => {
                     return (
                         <div className="single-blogs" key={item.id}>
-                            <div className="title">Title: {item.title}</div>
+                            <div className="title">
+                                <span> Title: {item.title}</span>
+                                <span onClick={() => deletePost(item.id)}> X</span>
+                            </div>
                             <div className="content">Body: {item.body}</div>
                             <button>
                                 <Link to={`/blog/${item.id}`}>View Detail</Link>
@@ -42,3 +78,4 @@ const Blog = () => {
     )
 }
 export default Blog;
+
